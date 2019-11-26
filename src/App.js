f0f8824f827f4d1ee48ac00b56376f0e08583865
Lawal Samuel <samthedonz@gmail.com>
2019-11-26 05:27:54 -0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
+
 import { Switch, Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop-page/shop-page.component'
+
 import Header from './components/header/header.component'
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); 
 
     this.state = {
       currentUser: null
--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 
 import { BrowserRouter } from 'react-router-dom'
 
+import { Provider } from 'react-redux';
+
+import store from './redux/store'
+
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
